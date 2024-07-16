48813c1f57ecf78ae05191b362c35763ebec230e
imgiang <79862204+imgiang@users.noreply.github.com>
2024-07-16 15:17:56 +0700

    add check correct btn

--- js/function.js
+++ js/function.js
@@ -1,4 +1,7 @@
-const init = function(){
+var v1 = [];
+var v1Input = [];
+var v2 = [];
+const init = function () {
     // Create the floating button
     const floatingButton = document.createElement('div');
     floatingButton.id = 'floatingButton';
@@ -8,13 +11,54 @@ const init = function(){
     document.body.appendChild(floatingButton);
 
     // Add event listener to the floating button
-    floatingButton.addEventListener('click', function() {
-        var t ="";
+    floatingButton.addEventListener('click', function () {
+        v1 = [];
+        v2 = [];
+        v1Input = [];
         const nodeList = document.querySelectorAll("div.DoDictationAudioAfterLogin__Body-sc-1ufhija-3");
+        nodeList.forEach(element => {
+            const children = element.children;
 
-        // nodeList[0].children
+            // Loop through each child element
+            for (let child of children) {
+                const inputs = child.querySelectorAll('input');
+                const spans = child.querySelectorAll('span');
 
-        alert('Floating button clicked!'+ nodeList[0].children.length + " !");
+                inputs.forEach(input => {
+                    v1Input.push(input);
+                    v1.push(input.value);
+                });
+                spans.forEach(span => {
+                    v1Input.push(span);
+                    v1.push(span.innerHTML);
+                });
+            }
+        });
+
+        // get v2
+        const nodeList2 = document.querySelectorAll(".SectionViewScriptDetail__Content-sc-uknfvf-2.jXPNsu");
+        const wordList = [];
+        for (let child of nodeList2[0].children) {
+            const spans = child.querySelectorAll('span');
+            spans.forEach(span => {
+                wordList.push(span.innerHTML);
+            });
+        }
+        v2 = wordList[1].split(/(\.)/).filter(Boolean);
+        v2 = v2.flatMap(item => item.split(/\s+/)).filter(Boolean);
+
+        // compare two array
+        for (let i = 0; i < v2.length; i++) {
+            if (v1[i].toLowerCase() !== v2[i].toLowerCase()) {
+                console.log(v1Input[i]);
+                console.log(v1Input[i].classList);
+                v1Input[i].classList.add('InCorrectColor');
+            } else {
+                console.log(v1Input[i]);
+                console.log(v1Input[i].classList);
+                v1Input[i].classList.add('CorrectColor');
+            }
+        }
     });
 
     const floatingButton2 = document.createElement('div');
@@ -25,7 +69,7 @@ const init = function(){
     document.body.appendChild(floatingButton2);
 
     // Add event listener to the floating button
-    floatingButton2.addEventListener('click', function() {
+    floatingButton2.addEventListener('click', function () {
         alert('Floating button clicked!');
     });
 }
